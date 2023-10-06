2275d4ad90589ef06e28c199539c018e8b21434f
JudahAvadom <judaslinarez@gmail.com>
2023-10-05 23:07:31 -0500

    inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className='p-4'>
-      <div
-        style={{textAlign:'-webkit-center'}}
-      >
-        <Image
-          src="/ash.png"
-          width={100}
-          height={100}
-          alt="Ash logo"
-        />
-      </div>
       <h1 className="text-3xl text-white text-center">Sean Bienvenidos a ASH</h1>
     </div>
   )
